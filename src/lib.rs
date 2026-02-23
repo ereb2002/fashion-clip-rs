40c7b23ceeaacbf092ee320f12a4e4456c48badf
ereb2002 <ereb2002@gmail.com>
2026-02-22 23:18:51 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
 pub mod embed;
 pub mod config;
 pub mod clip_image_processor;
+
+pub mod error;
+
+pub use embed::{EmbedImage, EmbedText};
+pub use error::Error;
+
+pub mod prelude {
+    pub use crate::embed::{EmbedImage, EmbedText};
+}
